25c8642172cf2fcd1f54c6d2297345b25d40b531
fiba-dev <fibarrola06@gmail.com>
2022-02-06 09:42:31 -0400

    change webpackConfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const path = require("path");
 const dev = process.env.NODE_ENV == "development";
-const nodeExternals = require('webpack-node-externals');
-
 const liveServer = require("live-server");
-
-console.log("liveserverports",liveServer);
 if (dev) {
 	liveServer.start({
 		root: "./",
 		file: "index.html",
 	});
-};
-const common = {
-	
-    module: {
-	
-        rules: [
+}
+module.exports = {
+	watch: dev,
+	entry: "./fe-src/index.tsx",
+	module: {
+		rules: [
 			{
 				test: /\.tsx?$/,
 				use: "ts-loader",
@@ -29,8 +25,7 @@ const common = {
 				}
 			} ],
 
-			},
-			{
+			},{
 				test: /\.jpe?g$|\.gif$|\.png$|\.PNG$|\.svg$|\.woff(2)?$|\.ttf$|\.eot$/,
 				loader: 'file-loader',
 				options: {
@@ -38,49 +33,12 @@ const common = {
 				}  
 			}
 		],
-    },
-    plugins: [ /* common plugins */ ],
-    resolve: {
-        extensions: [".tsx", ".js",".ts"] // common extensions
-    },
-
-    // other plugins, postcss config etc. common for frontend and backend
-};
-const frontend = {
-	
-    entry: "/fe-src/index.tsx",
-	
-    output: {
+	},
+	resolve: {
+		extensions: [".tsx", ".js",".ts"],
+	},
+	output: {
 		path: path.resolve(__dirname, "dist"),
-       filename: 'frontend-output.js'
-    },
-	
-
-    // other loaders, plugins etc. specific for frontend
+		filename: "bundle.js",
+	},
 };
-const backend = {
-	
-    entry:"/be-src/index.ts",
-	
-    output: {
-		path: path.resolve(__dirname, "dist"),
-       filename: 'backend-output.js',
-	   
-    },
-	
-	
-	target: 'node',
-	
-    externalsPresets: { node: true },
-    externals:[nodeExternals()],
-
-    // specify for example node_modules to be not bundled
-    // other loaders, plugins etc. specific for backend
-};
-module.exports =[ Object.assign({} , common, frontend),
-    Object.assign({} , common, backend),
-   
-   
-]
-
-
