adea047cec457d34af47cbe40696ce1567a1f764
fiba-dev <fibarrola06@gmail.com>
2022-04-27 07:34:43 -0400

    add aria label

--- src/components/ui/buttons/index.tsx
+++ src/components/ui/buttons/index.tsx
@@ -3,64 +3,88 @@ import css from "./index.css";
 import { Link } from "react-router-dom";
 
 function Button(props) {
-  return (
-    <button onClick={props.onClick} className={css.boton}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			onClick={props.onClick}
+			aria-label="Boton general"
+			className={css.boton}
+		>
+			{props.children}
+		</button>
+	);
 }
 function CancelButton(props) {
-  return (
-    <button className={css.cancel__button} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			className={css.cancel__button}
+			aria-label="Cancelar"
+			onClick={props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 }
 
 function MenuButton(props) {
-  return (
-    <button className={css.button__menu} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			className={css.button__menu}
+			aria-label="Menu"
+			onClick={props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 }
 
 function CloseButton(props) {
-  return (
-    <button className={css.close__menu} onClick={props.onClick}>
-      X
-    </button>
-  );
+	return (
+		<button
+			className={css.close__menu}
+			aria-label="Cerrar Ventana"
+			onClick={props.onClick}
+		>
+			X
+		</button>
+	);
 }
 function SessionButton(props) {
-  return (
-    <button className={css.session} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			className={css.session}
+			aria-label="Boton de sesion"
+			onClick={props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 }
 function UnpublishButton(props) {
-  return (
-    <button className={css.unpublish} onClick={props.onClick}>
-      {" "}
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			className={css.unpublish}
+			aria-label="eliminar publicacion"
+			onClick={props.onClick}
+		>
+			{" "}
+			{props.children}
+		</button>
+	);
 }
 function FoundButton(props) {
-  return (
-    <button className={css.found} onClick={props.onClick}>
-      {" "}
-      {props.children}
-    </button>
-  );
+	return (
+		<button className={css.found} aria-label="Buscar" onClick={props.onClick}>
+			{" "}
+			{props.children}
+		</button>
+	);
 }
 export {
-  Button,
-  MenuButton,
-  CloseButton,
-  SessionButton,
-  CancelButton,
-  UnpublishButton,
-  FoundButton,
+	Button,
+	MenuButton,
+	CloseButton,
+	SessionButton,
+	CancelButton,
+	UnpublishButton,
+	FoundButton,
 };
