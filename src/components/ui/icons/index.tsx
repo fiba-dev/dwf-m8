2000fd3a75196623b27d7953ffbf444a8f14df65
fiba-dev <fibarrola06@gmail.com>
2022-04-27 07:43:41 -0400

    add aria label headers

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -3,22 +3,37 @@ import css from "./index.css";
 import { Link } from "react-router-dom";
 
 function IconMobile(props) {
-  return (
-    <button className={css.icon__mobile} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			aria-label="Boton Nav Mobile"
+			className={css.icon__mobile}
+			onClick={props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 }
 function IconDesktop(props) {
-  return (
-    <button onClick={props.onClick} id={props.id} className={css.icon__desktop}>
-      {props.children}
-    </button>
-  );
+	return (
+		<button
+			aria-label="Boton Nav Desktop"
+			onClick={props.onClick}
+			id={props.id}
+			className={css.icon__desktop}
+		>
+			{props.children}
+		</button>
+	);
 }
 
 function IconStart(props) {
-  return <button className={css.header__logo} onClick={props.onClick}></button>;
+	return (
+		<button
+			className={css.header__logo}
+			aria-label="Menu Boton"
+			onClick={props.onClick}
+		></button>
+	);
 }
 
 export { IconMobile, IconDesktop, IconStart };
